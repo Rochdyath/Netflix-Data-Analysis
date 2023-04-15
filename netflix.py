9473d14286e07edb47d225922b7caf6dad38ec59
Rochdyath <rochdiath.bachabi@epitech.eu>
2023-04-15 16:26:57 +0100

    feat: distribution of content ratings

--- netflix.py
+++ netflix.py
@@ -4,6 +4,14 @@ import plotly.express as px # for data visualization
 from textblob import TextBlob # for sentiment analysis
 
 dff = pd.read_csv('netflix_titles.csv')
-print(dff.shape)
-print(dff.columns)
-# ()
+# print(dff.shape)
+# print(dff.column)
+
+def distribution_of_content_ratings():
+    z = dff.groupby(['rating']).size().reset_index(name='counts')
+    pieChart = px.pie(z, values='counts', names='rating', 
+                    title='Distribution of Content Ratings on Netflix',
+                    color_discrete_sequence=px.colors.qualitative.Set3)
+    pieChart.show()
+
+distribution_of_content_ratings()
